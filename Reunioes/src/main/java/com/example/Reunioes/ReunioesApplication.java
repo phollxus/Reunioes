ee066e3dfaa114385e328bb764109c26c65cd12b
phollxus <phollguitselar@hotmail.com>
2021-08-28 09:49:48 -0300

    commit inicial

---
+++ Reunioes/src/main/java/com/example/Reunioes/ReunioesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReunioesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReunioesApplication.class, args);
+	}
+
+}
